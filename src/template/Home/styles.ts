1f397660b6c805b16e0bfef3afafc60e34118ff2
anna-cardoso <annakaroline2021@usp.br>
2022-10-24 15:30:17 -0300

    add

--- src/template/Home/styles.ts
+++ src/template/Home/styles.ts
@@ -1,14 +1,35 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    font-size: 2rem;
-    color: black;
-`;
-
-export const Container = styled.div`
+export const MainBody = styled.div`
+    min-height: 100%;
+    width: 100%;
+    height: 100%;
     display: flex;
-    height: 100vh;
+    align-items: space-between;
+`;
+export const SideBarContainer = styled.div`
+    display: flex;
+    height: 100%;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    width: 18%;
+    background-color: #272727;
+    border-right-width: 2px solid #55596b;
+    left: 0px;
+    position: fixed;
+`;
+export const NavBarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    padding: 0p 56p 0px 0px;
+    height: 336px;
+`;
+export const ProfileContainer = styled.div`
+    display: flex;
+    position: bottom;
+    margin-top: 72px;
+    padding: 64px 128px 64px128px;
+    flex-direction: column;
+    height: 192px;
 `;
